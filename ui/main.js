571d2bf95a1501f7d30dc42f9818886cc24b749f
ragavendra <ragavendradevraj@gmail.com>
2016-11-19 21:51:57 +0530

    [imad-console] Updates ui/main.js

---
+++ ui/main.js
@@ -0,0 +1,12 @@
+var img = document.getElementById('img');
+var marginLeft = 0;
+function moveRight(){
+         marginLeft = marginLeft+1;
+         img.style.marginLeft = marginLeft+'px';
+         
+}
+img.onclick =function(){
+      var interval = setInterval(moveRight,50);
+      
+};
+
