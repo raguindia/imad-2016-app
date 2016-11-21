29459c08eaec796c6f6f96a7007f81d8f61e6f40
ragavendra <ragavendradevraj@gmail.com>
2016-11-21 09:18:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,10 @@
-console.log('loaded');
+$(document).ready(function() {
+  var deck = new $.scrolldeck({
+    buttons: '.nav-button',
+    slides: '.slide',
+    duration: 600,
+    easing: 'easeInOutExpo',
+             offset: 0
+  });
+});
 
