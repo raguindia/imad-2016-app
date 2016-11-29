75332c67c751ddaac1976b5d7d62671141992832
ragavendra <ragavendradevraj@gmail.com>
2016-11-29 18:17:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/', function (req, res) {
+app.get('/profile', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'profile.html'));
 });
-app.get('/', function (req, res) {
+app.get('/blog', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'blog.html'));
 });
 
