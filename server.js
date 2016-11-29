ed0389ebe6d74c265dcd8f5f1dc450264a4589aa
ragavendra <ragavendradevraj@gmail.com>
2016-11-29 17:57:08 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'profile.html'));
+});
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'blog.html'));
+});
+app.get('/ui/sytle', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+app.listen(8080, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+});
+
